3a16f539ab048b77d051d2cc1034bd0c4824f75d
Wahyu Pinanda Ginting <84735060+WahyuPinanda@users.noreply.github.com>
2024-12-03 17:53:28 +0800

    Revert "feature/midtrans"

--- app.js
+++ app.js
@@ -1,17 +1,53 @@
-require('dotenv').config();
-const cors = require('cors')
-const express = require('express');
-const errorHandler = require('./middlewares/errorHandler');
-const paymentRoutes = require('./routes/paymentRoutes');
+require("dotenv").config();
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+const prisma = new PrismaClient();
 
-app.use(cors());
+//routers
+const ticketListingRouter = require("./routers/flightsRouter");
+const paginationRouter = require('./routers/paginationRouter')
+const userRouter = require('./routers/userRouter')
+const seatRouter = require('./routers/seatRouter')
+const airlineRouter = require("./routers/airlineRouter");
+const passwordRouter = require("./routers/passwordRouter");
+
 app.use(express.json());
-app.use('/api', paymentRoutes);
-app.use(errorHandler);
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const routers = [
+  ticketListingRouter,
+  paginationRouter,
+  userRouter,
+  seatRouter,
+  airlineRouter,
+  passwordRouter,
+];
+
+routers.forEach(router => app.use('/api/v1', router));
+
+//buat nangkep semua error langsung
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    status: false,
+    message : "Lihat error di console"
+  })
+})
+
+// Sample query
+// app.get("/users", async (req, res) => {
+//   const users = await prisma.user.findMany();
+//   res.json(users);
+// });
+
+// Start Server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
--- routes/paymentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createPaymentController, handleNotificationController } = require('../controllers/paymentController');
-
-const router = express.Router();
-
-router.post('/payments', createPaymentController);
-router.post('/payments/notification', handleNotificationController);
-
-module.exports = router;

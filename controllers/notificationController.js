0b97f432d7db7f0622c26c1b6f75bc2bfb8fcc4e
af1fnt <afifnaufalt@gmail.com>
2024-12-04 15:09:44 +0700

    feat: add notification controller (create)

---
+++ controllers/notificationController.js
@@ -0,0 +1,25 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+class NotificationController {
+
+  static async createNotification(req, res, next) {
+    const { title, description, user_id } = req.body;
+    try {
+      const notification = await prisma.notification.create({
+        data: { title, description, user_id: parseInt(user_id) },
+      });
+      res.status(201).json({
+        status: "success",
+        message: "Notification created successfully",
+        data: notification,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+
+}
+
+module.exports = NotificationController;

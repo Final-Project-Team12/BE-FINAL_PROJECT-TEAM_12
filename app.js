2dc034beb08f93a7d55b52ea7b90b607dd2bd136
zefanyadiegoforlandi <zddiiego@gmail.com>
2024-12-06 20:07:42 +0700

    fix: fixed route

--- app.js
+++ app.js
@@ -75,5 +75,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT} zefanya tanpa pemanis buatan`);
 });

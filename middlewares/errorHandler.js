7bf7cff8aff26f3b70948cda4e4cdc85709d44ca
zefanyadiegoforlandi <zddiiego@gmail.com>
2024-12-06 12:49:33 +0700

    fix: fixed bug response

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,10 @@
 function errorHandler(err, req, res, next) {
   console.error(err);
-  res.status(500).json({ message: 'Internal Server Error', error: err.message });
+  res.status(500).json({ 
+    status: 'fail',
+    statusCode: 500,
+    message: 'Internal Server Error', 
+    error: err.message });
 }
 
 module.exports = errorHandler;

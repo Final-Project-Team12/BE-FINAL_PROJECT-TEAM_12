0d93b173141858ffed57c7246afe74e15ce32583
zefanyadiegoforlandi <zddiiego@gmail.com>
2024-12-06 16:36:34 +0700

    fix: fixed route airport to restrictJwt

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ router.post('/user/verify', async (req, res, next) => {UserController.verifyUser
 router.post('/user/resend', async (req, res, next) => {UserController.resendOtp(req, res, next)})
 router.post('/user/login', async (req, res, next) => {UserController.login(req, res, next)})
 
-// router.use(restrictJwt)
+router.use(restrictJwt)
 
 router.get('/user/:user_id', async (req, res, next) => {UserController.getUser(req, res, next, req.params.user_id)})
 router.put('/user/:user_id', async (req, res, next) => {UserController.updateUser(req, res, next, req.params.user_id)})

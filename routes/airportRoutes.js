6ee5c52b7c7a21beceb74c96d158a7b736cfb725
zefanyadiegoforlandi <zddiiego@gmail.com>
2024-12-06 16:34:07 +0700

    fix: fixed router and app.js

--- routes/airportRoutes.js
+++ routes/airportRoutes.js
@@ -2,13 +2,12 @@ const express = require('express');
 const multer = require('multer');
 const AirportController = require('../controllers/airportController');
 const router = express.Router();
+const multerUpload = require('../libs/multerUpload');
 
-const upload = multer({ storage: multer.memoryStorage() });
-
-router.post('/upload', upload.single('image'), AirportController.uploadImageAirport);
-router.get('/', AirportController.getAirports);
+router.post('/airport/upload', multerUpload.single('image'), AirportController.uploadImageAirport);
+router.get('/airport', AirportController.getAirports);
 router.get('/:airport_id', AirportController.getAirportById);
 router.delete('/:airport_id', AirportController.deleteAirport);
-router.put('/:airport_id', upload.single('image'), AirportController.updateAirport);
+router.put('/:airport_id', multerUpload.single('image'), AirportController.updateAirport);
 
 module.exports = router;

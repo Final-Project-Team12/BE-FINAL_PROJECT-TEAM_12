9eed1ab0a3f080189f473a4c524de3c29216c93f
CornellBK <102948950+CornellBK@users.noreply.github.com>
2024-12-10 11:54:38 +0700

    fix, refactor
    fix validation user register
    refactor userController.js, restrictJwt.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,14 @@
 require("dotenv").config();
-const { PrismaClient } = require("@prisma/client");
-const host = process.env.HOST;
 const joi = require('joi');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Mailer = require('../libs/mailer');
 const crypto = require('crypto');
 
+const { getUserByEmail, updateUserByEmail, createUser, updateUserById, getUserById, deleteUserById, checkOtherEmail } = require("../services/userService");
+
 const HASH = process.env.HASH;
 const JWT_SECRET = process.env.JWT_SECRET;
-const prisma = new PrismaClient();
 
 const login_schema = joi.object({
   email: joi.string().email().required(),
@@ -38,6 +37,25 @@ const user_schema = joi.object({
     // role: joi.string().valid('user', 'admin', 'moderator').required(),
   });
 
+const user_update_schema = joi.object({
+  name: joi.string().min(3).max(50),
+  telephone_number: joi.string()
+    .pattern(/^[0-9]+$/)
+    .min(10)
+    .max(15),
+  email: joi.string().email(),
+  password: joi.string()
+    .min(8)
+    .max(128),
+  //   .pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/),
+  address: joi.string().max(255),
+  gender: joi.string()
+    .valid('male', 'female'),
+  identity_number: joi.string().alphanum().min(5).max(20),
+  age: joi.number().integer().min(1).max(100),
+  // role: joi.string().valid('user', 'admin', 'moderator').required(),
+});
+
 class UserController{
     static async genOtp(email, next){
       try{
@@ -63,11 +81,7 @@ class UserController{
           });
       }
       try{
-        let userData = await prisma.users.findUnique({
-          where: {
-              email: value.email
-          }
-        })
+        let userData = await getUserByEmail(value.email);
     
         if(!userData){
           return res.status(401).json({
@@ -89,14 +103,12 @@ class UserController{
 
             await Mailer.sendVerificationEmail(value.email, otp);
 
-            const userData = await prisma.users.update({
-              where: {email:value.email},
-              data: {
-                otp,
-                otp_expiry: otpExpiry
-              }
-            })
+            const data = {
+              otp,
+              otp_expiry: otpExpiry
+            }
 
+            const userData = await updateUserByEmail(value.email, data)
 
             return res.status(200).json({
               status: true,
@@ -123,11 +135,8 @@ class UserController{
             }
 
             let password = bcrypt.hashSync(value.password, parseInt(HASH));
-            const cekEmailUnik = await prisma.users.findUnique({
-              where: {
-                  email : value.email
-              }
-            })
+            const cekEmailUnik = await getUserByEmail(value.email)
+
             if(cekEmailUnik){
               return res.status(400).json({
                   status : false,
@@ -135,24 +144,23 @@ class UserController{
               })
             }
             
-            let { otpGen, otpExpiry } = await this.genOtp(value.email, next);
-            console.log(otpGen);
+            let { otpGen, otpExpiry } = await UserController.genOtp(value.email, next);
             
-            const newUser = await prisma.users.create({
-                data: {
-                  name: value.name,
-                  telephone_number: value.telephone_number,
-                  email: value.email,
-                  password: password,
-                  address: value.address,
-                  gender: value.gender,
-                  identity_number: value.identity_number,
-                  age: value.age,
-                  role: 'user',
-                  otp : otpGen,
-                  otp_expiry: otpExpiry
-                },
-            });
+            const data = {
+              name: value.name,
+              telephone_number: value.telephone_number,
+              email: value.email,
+              password: password,
+              address: value.address,
+              gender: value.gender,
+              identity_number: value.identity_number,
+              age: value.age,
+              role: 'user',
+              otp : otpGen,
+              otp_expiry: otpExpiry
+            }
+
+            const newUser = await createUser(data);
 
             const { otp, ...userWithoutOtp } = newUser;
             
@@ -178,7 +186,7 @@ class UserController{
       }
 
       try {
-        const user = await prisma.users.findUnique({ where: { email } });
+        const user = await getUserByEmail(email);
   
         if (!user) {
           return res.status(404).json({ 
@@ -198,14 +206,13 @@ class UserController{
           });
         }
   
-        let userData = await prisma.users.update({
-          where: { email },
-          data: {
-            verified: true, 
-            otp: null, 
-            otp_expiry: null
-          },
-        });
+        const data = {
+          verified: true, 
+          otp: null, 
+          otp_expiry: null
+        };
+
+        let userData = await updateUserByEmail(email, data);
   
         return res.status(200).json({
           status: true,
@@ -218,13 +225,11 @@ class UserController{
       }      
     }
 
-    static async getUser(req, res, next, user_id){
+    static async getUser(req, res, next){
+      const user_id = req.params.user_id;
         try{
-            let user = await prisma.users.findUnique({
-                where: {
-                    user_id : parseInt(user_id)
-                }
-            })
+            let user = await getUserById(parseInt(user_id));
+            
             if(user){
                 return res.status(200).json({
                     status: true,
@@ -244,84 +249,81 @@ class UserController{
             return;
         }
     }
-    static async updateUser(req, res, next, user_id){
-        try{
-            const { error, value } = user_schema.validate(req.body);
-            if (error) {
-                return res.status(400).json({
-                    status: false,
-                    message: 'input error',
-                    error: error.details[0].message
-                });
-            }
-            let password = bcrypt.hashSync(value.password, parseInt(HASH));
-            const cekEmailUnik = await prisma.users.findUnique({
-              where: {
-                  email : value.email
-              }
-            })
-            if(cekEmailUnik){
-              return res.status(400).json({
-                  status : false,
-                  message: "Email already exists"
-              })
-            }
-            
-            const new_user = await prisma.users.update({
-                where: {
-                    user_id: parseInt(user_id)
-                },
-                data: {
-                  name: value.name,
-                  telephone_number: value.telephone_number,
-                  email: value.email,
-                  password: password,
-                  address: value.address,
-                  gender: value.gender,
-                  identity_number: value.identity_number,
-                  age: value.age,
-                  role: 'user',
-                },
+    static async updateUser(req, res, next){
+      const user_id = req.params.user_id;
+      try{
+        const { error, value } = user_update_schema.validate(req.body);
+        if (error) {
+            return res.status(400).json({
+                status: false,
+                message: 'input error',
+                error: error.details[0].message
             });
-            
-            return res.status(200).json({
-                status: true,
-                message: "success",
-                data: new_user
-            });
-          }
-          catch (error) {
-            next(error);
-            return;
-          }
-    }
-    static async deleteUser(req, res, next, user_id){
-        try{
-            const existingUser = await prisma.users.findUnique({
-                where: {
-                  user_id: parseInt(user_id),
-                },
-            });
-            if (!existingUser) {
-                return res.status(404).json({
-                  status: false,
-                  message: "User not found",
-                });
-              }
-
-            const deletedUser = await prisma.users.delete({
-                where: {
-                    user_id: parseInt(user_id)
-                }
-            }) 
-            
-            return res.status(200).json({
-                status: true,
-                message: "success",
-                data: deletedUser
-            })
-            
         }
+
+        const existingUser = await getUserById(parseInt(user_id));
+        if (!existingUser) {
+          return res.status(404).json({
+              status: false,
+              message: 'User not found',
+          });
+      }
+
+        let password = req.body.password? bcrypt.hashSync(value.password, parseInt(HASH)) : existingUser.password
+        const cekEmailUnik = await checkOtherEmail(value.email)
+        if(cekEmailUnik){
+          return res.status(400).json({
+              status : false,
+              message: "Email already used for another account"
+          })
+        }
+
+        const data = {
+          name: req.body.name || existingUser.name,
+          telephone_number: req.body.telephone_number || existingUser.telephone_number,
+          email: req.body.email || existingUser.email,
+          password: req.body.password
+              ? password
+              : existingUser.password,
+          address: req.body.address || existingUser.address,
+          gender: req.body.gender || existingUser.gender,
+          identity_number: req.body.identity_number || existingUser.identity_number,
+          age: req.body.age || existingUser.age,
+          role: "user"
+      };
+
+        const new_user = await updateUserById(parseInt(user_id), data)
+        
+        return res.status(200).json({
+            status: true,
+            message: "success",
+            data: new_user
+        });
+      }
+      catch (error) {
+        next(error);
+        return;
+      }
+    }
+    static async deleteUser(req, res, next){
+      const user_id = req.params.user_id;
+      try{
+          const existingUser = await getUserById(parseInt(user_id))
+          if (!existingUser) {
+              return res.status(404).json({
+                status: false,
+                message: "User not found",
+              });
+            }
+
+          const deletedUser = await deleteUserById(parseInt(user_id));
+          
+          return res.status(200).json({
+              status: true,
+              message: "success",
+              data: deletedUser
+          })
+      }
         catch(error){
             next(error);
             return;
@@ -340,11 +342,7 @@ class UserController{
       try{
         let {email, password} = value;
         // console.log('masuk g ni')
-        let userData = await prisma.users.findUnique({
-            where: {
-                email
-            }
-        })
+        let userData = await getUserByEmail(email);
     
         if(!userData){
           return res.status(401).json({

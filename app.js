376c14c4ed152c3b2465637a59e4da2e6c368503
Wahyu Pinanda Ginting <Keiwahyu19@gmail.com>
2024-12-10 23:37:32 +0800

    fix cors

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ const paymentRoutes = require('./routes/paymentRoutes');
 const notificationRoutes = require("./routes/notificationRoutes");
 const transactionRoutes = require('./routes/transactionsRoutes');
 const ticketRoutes = require('./routes/ticketsRoutes');
-app.use(cors());
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

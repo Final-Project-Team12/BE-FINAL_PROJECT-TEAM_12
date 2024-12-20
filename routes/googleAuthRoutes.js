00db482cc7ef965724d40beac679ca64d2346b90
zefanyadiegoforlandi <zddiiego@gmail.com>
2024-12-21 00:15:18 +0700

    fix: fixed ooauth

--- routes/googleAuthRoutes.js
+++ routes/googleAuthRoutes.js
@@ -3,8 +3,8 @@ const GoogleAuthController = require('../controllers/googleAuthController');
 
 const router = express.Router();
 
-router.get('/google', GoogleAuthController.googleLogin);
-router.get('/google/callback', GoogleAuthController.googleCallback);
-router.put('/google/password/', GoogleAuthController.updatePassword);
+router.get('auth/google', GoogleAuthController.googleLogin);
+router.get('auth/google/callback', GoogleAuthController.googleCallback);
+router.put('auth/google/password/', GoogleAuthController.updatePassword);
 
 module.exports = router;
